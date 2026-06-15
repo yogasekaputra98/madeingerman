366be254bb28514a74efb11a1c06804e810c937d
Yogasekaputra <yogasekaputra@gmail.com>
2026-06-15 10:51:05 +0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+import os
+
+DB_CONFIG = {
+    "host": "db.uhu.local",
+    "port": 5432,
+    "database": "stic_kuning_dev",
+    "username": "admin_stic",
+    "password": "UhuSTICKuning2026!"
+}
+
+print("Connected to database:", DB_CONFIG["database"])
